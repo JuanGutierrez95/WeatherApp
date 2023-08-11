ca71b520c77b69eae236de2e8b3b8b3b3bc6b904
JuanGutierrez95 <juan.gutierrez19958@gmail.com>
2023-08-10 23:55:57 -0300

    commit

---
+++ pages/index.tsx
@@ -0,0 +1,26 @@
+export default function Home() {
+  return (
+    <main className="flex justify-center items-center bg-gradient-to-br from-sky-400 via-rose-400 to-lime-400 h-[100vh] w-full">
+      <section className="w-full md:max-w-[500px] p-4 flex flex-col text-center items-center justify-center md:px-10 lg:p-24 h-full lg:h-[500px] bg-white bg-opacity-20 backdrop-blur-ls rounded drop-shadow-lg text-zinc-700">
+        <h1 className="mb-4 text-4xl font-bold">Weather Forecast</h1>
+        <p className="mt-2 mb-4 text-sm">
+          Enter below a place you want to know the weather of and select an
+          option from the dropdown
+        </p>
+
+      <div className="flex mt-10 md:mt-4">
+        
+        <input
+          type="text"
+          value={""}
+          className="px-2 py-1 border-2 border-white rounded-l-md"
+          />
+
+        <button className="px-2 py-1 border-2 cursor-pointer rounded-r-md border-zinc-100 hover:border-zinc-500 hover:text-zinc-500 text-zinc-100">
+          Search
+        </button>
+          </div>
+      </section>
+    </main>
+  );
+}
