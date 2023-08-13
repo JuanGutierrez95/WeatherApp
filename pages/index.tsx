da5f0b832b24231a70cc0cf20518e028926d7930
JuanGutierrez95 <juan.gutierrez19958@gmail.com>
2023-08-13 05:53:39 -0300

    new changes

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,7 +6,7 @@ const Home: React.FC = () => {
   const [search, setSearch] = useState<string>("");
   const [options, setOptions] = useState<[]>([]);
   const [city, setCity] = useState<Option | null>(null);
-
+  const [forecast, setForecast] = useState<null>(null);
   const getSearch = (value: string) => {
     fetch(
       `http://api.openweathermap.org/geo/1.0/direct?q=${value.trim()}&limit=5&appid=${
@@ -34,7 +34,7 @@ const Home: React.FC = () => {
       `http://api.openweathermap.org/data/2.5/weather?lat=${city.lat}&lon=${city.lon}&units=metric&appid=${process.env.NEXT_PUBLIC_API_KEY}`
     )
       .then((res) => res.json())
-      .then((data) => console.log({ data }));
+      .then((data) => setForecast(data));
   }
 
 
@@ -59,13 +59,17 @@ const Home: React.FC = () => {
 
   return (
     <main className="flex justify-center items-center bg-gradient-to-br from-sky-400 via-rose-400 to-lime-400 h-[100vh] w-full">
+      (forecast ? (
+
+      ):
       <Search
         search={search}
         options={options}
         handleSearchChange={handleSearchChange}
         optionSelect={optionSelect}
         handleSubmit={handleSubmit} 
-      />
+        />
+        )
     </main>
   );
 };
