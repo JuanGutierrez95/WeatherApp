2f04665a8604b0f4f20024575654f5a366ba0f56
JuanGutierrez95 <juan.gutierrez19958@gmail.com>
2023-08-17 03:31:46 -0300

    new changes

---
+++ icons/Barometer.tsx
@@ -0,0 +1,10 @@
+import {WiBarometer} from 'react-icons/wi'
+const Barometer = () => {
+  return (
+    <div>
+        <WiBarometer />
+    </div>
+  )
+}
+
+export default Barometer
---
+++ icons/Visibility.tsx
@@ -0,0 +1,10 @@
+import {MdOutlineVisibility} from 'react-icons/md'
+const Visibility = () => {
+  return (
+    <div>
+        <MdOutlineVisibility />
+    </div>
+  )
+}
+
+export default Visibility
