d75c6076f17b8971879339464be38da900cd23af
JuanGutierrez95 <juan.gutierrez19958@gmail.com>
2023-08-17 02:43:10 -0300

    new changes

--- components/Forecast.tsx
+++ components/Forecast.tsx
@@ -1,15 +1,65 @@
-import { Forecast } from "@/types/types"
+import { Forecast } from "@/types/types";
+import Image from "next/image";
 
 type PropsForecast = {
-    data: Forecast
-}
+  data: Forecast;
+};
 
-const Forecast = ({data} : PropsForecast) : JSX.Element => {
+const Degree = ({ temp }: { temp: number }): JSX.Element => (
+  <span>
+    {temp}
+    <sup>°</sup>
+  </span>
+);
+
+const Forecast = ({ data }: PropsForecast): JSX.Element => {
+  const today = data.list[0];
+  console.log({ today });
+
+const image = `https://openweathermap.org/img/wn/${today.weather[0].icon}.png`;
   return (
-    <div>
-        <p>Forecast</p>
-    </div>
-  )
-}
+    <div
+      className="w-full md:max-w-[500px] py-4 md:py-4
+    md:px-10 lg:px-24 h-full lg:h-auto bg-white bg-opacity-20
+    backdrop-blur-ls rounded drop-shadow-lg"
+    >
+      <div className="mx-auto w-[300px]">
+        <section className="text-center">
+          <h2 className="text-2xl font-black">{data.name}</h2>
+          <span className="font-thin">{data.country}</span>
+          <h1 className="text-4xl font-extralight">
+            <Degree temp={Math.round(today.main.temp)} />
+          </h1>
+          <p> {today.weather[0].main} </p>
+          <p className="text-sm">
+            Max: <Degree temp={Math.ceil(today.main.temp_max)} /> Min:{" "}
+            <Degree temp={Math.floor(today.main.temp_min)} />
+          </p>
+        </section>
 
-export default Forecast
+        <section className="flex pb-2 mt-4 mb-5 overflow-x-scroll">
+          {data.list.map((item, index) => (
+            <div className="inline-block text-center w-[50px] flex-shrink-0" key={index}>
+              <p className="text-sm">
+                {
+                  index === 0 ? 'Now' : new Date(item.dt * 1000).getHours() + ':00'
+                }
+                </p>
+              <Image
+                src={`http://openweathermap.org/img/wn/${item.weather[0].icon}@2x.png`}
+                width={50}
+                height={50}
+                alt={`weather-icon-${item.weather[0].description}`}
+              />
+              <p className="text-sm font-bold">
+                <Degree temp={Math.round(item.main.temp)} />
+              </p>
+            </div>
+          ))}
+        </section>
+      </div>
+    </div>
+  );
+};
+
+export default Forecast;
